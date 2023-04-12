dbbb4071f8bf4569d346a79b75c533f41a5ebb96
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-12 15:13:57 -0300

    criando a rota do DefaultLayout da aplicação

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/Home'
 import { CheckoutPage } from './pages/Checkout'
 import { SuccessPage } from './pages/Success'
+import { DefaultLayout } from './layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/success" element={<SuccessPage />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/success" element={<SuccessPage />} />
+      </Route>
     </Routes>
   )
 }

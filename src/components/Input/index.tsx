85f0e3517d824b3585a17b046f7be275d4ce949f
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 19:56:11 -0300

    adicionando o forwardRef no Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,8 +1,9 @@
-import { InputHTMLAttributes } from 'react'
+import { InputHTMLAttributes, forwardRef } from 'react'
 import { InputStyleContainer } from './styles'
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>
 
-export function Input({ ...props }: InputProps) {
-  return <InputStyleContainer {...props} />
-}
+// eslint-disable-next-line react/display-name
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return <InputStyleContainer {...props} ref={ref} />
+})

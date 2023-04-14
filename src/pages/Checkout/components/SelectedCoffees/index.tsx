46824452c30bab0d92c649450e1985b245ceea85
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 19:24:24 -0300

    retornando a lista de cafés selecionados de forma dinâmica

--- src/pages/Checkout/components/SelectedCoffees/index.tsx
+++ src/pages/Checkout/components/SelectedCoffees/index.tsx
@@ -1,18 +1,21 @@
 import { TitleText } from '../../../../components/Typography'
+import { useCart } from '../../../../hooks/useCart'
 import { CoffeeCartCard } from '../CoffeeCartCard'
 import { ConfirmationSection } from '../ConfirmationSection'
 import { DetailsContainer, SelectedCoffeeContainer } from './styles'
 
 export function SelectedCoffees() {
+  const { cartItems } = useCart()
+
   return (
     <SelectedCoffeeContainer>
       <TitleText size="xs" color="subtitle">
         Cafés Selecionados
       </TitleText>
       <DetailsContainer>
-        <CoffeeCartCard />
-        <CoffeeCartCard />
-        <CoffeeCartCard />
+        {cartItems.map((item) => {
+          return <CoffeeCartCard key={item.id} coffee={item} />
+        })}
         <ConfirmationSection />
       </DetailsContainer>
     </SelectedCoffeeContainer>

5fcde88e67b4f3b61d73daddc10716930f4173ca
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 19:26:05 -0300

    Passando as propriedades para o CoffeeCartCard

--- src/pages/Checkout/components/CoffeeCartCard/index.tsx
+++ src/pages/Checkout/components/CoffeeCartCard/index.tsx
@@ -3,20 +3,43 @@ import {
   CoffeeCartCardContainer,
   RemoveButton,
 } from './styles'
-import americanoCoffee from '../../../../../public/coffees/americano.png'
 import { RegularText } from '../../../../components/Typography'
 import { QuantityInput } from '../../../../components/QuantityInput'
 import { Trash } from 'phosphor-react'
+import { CartItem } from '../../../../contexts/CartContext'
+import { formatMoney } from '../../../../utils/formatMoney'
+import { useCart } from '../../../../hooks/useCart'
 
-export function CoffeeCartCard() {
+interface CoffeeCartCardProps {
+  coffee: CartItem
+}
+
+export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
+  const coffeeTotal = coffee.price * coffee.quantity
+  const formattedPrice = formatMoney(coffeeTotal)
+  const { changeCartItemQuantity } = useCart()
+
+  function handleIncrease() {
+    changeCartItemQuantity(coffee.id, 'increase')
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(coffee.id, 'decrease')
+  }
+
   return (
     <CoffeeCartCardContainer>
       <div>
-        <img src={americanoCoffee} alt="americano coffee" />
+        <img src={`/coffees/${coffee.photo}`} alt={coffee.name} />
         <div>
-          <RegularText color="subtitle">Expresso Tradicional</RegularText>
+          <RegularText color="subtitle">{coffee.name}</RegularText>
           <ActionsContainer>
-            <QuantityInput size="small" />
+            <QuantityInput
+              size="small"
+              quantity={coffee.quantity}
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+            />
             <RemoveButton>
               <Trash size={16} />
               Remover
@@ -24,7 +47,7 @@ export function CoffeeCartCard() {
           </ActionsContainer>
         </div>
       </div>
-      <p>R$ 9,90</p>
+      <p>{formattedPrice}</p>
     </CoffeeCartCardContainer>
   )
 }

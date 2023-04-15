1ee7d848bd5fd7a208e8c8f8c4397b957499f91c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 21:15:40 -0300

    criando a validação do form

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,12 +1,43 @@
 import { CompleteOrderForm } from './components/CompleteOrderForm'
 import { SelectedCoffees } from './components/SelectedCoffees'
 import { CompleteOrderContainer } from './styles'
+import * as zod from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm, FormProvider } from 'react-hook-form'
+
+const confirmOrderFormValidationSchema = zod.object({
+  cep: zod.string().min(1, 'Informe o CEP'),
+  street: zod.string().min(1, 'Informe a Rua'),
+  number: zod.string().min(1, 'Informe o Número'),
+  complement: zod.string(),
+  district: zod.string().min(1, 'Informe o Bairro'),
+  city: zod.string().min(1, 'Informe a Cidade'),
+  uf: zod.string().min(1, 'Informe o Estado'),
+})
+
+export type OrderData = zod.infer<typeof confirmOrderFormValidationSchema>
+
+type ConfirmOrderFormData = OrderData
 
 export function CheckoutPage() {
+  const confirmOrderForm = useForm<ConfirmOrderFormData>({
+    resolver: zodResolver(confirmOrderFormValidationSchema),
+  })
+  const { handleSubmit } = confirmOrderForm
+
+  function handleConfirmOrder(data: ConfirmOrderFormData) {
+    console.log(data)
+  }
+
   return (
-    <CompleteOrderContainer className="container">
-      <CompleteOrderForm />
-      <SelectedCoffees />
-    </CompleteOrderContainer>
+    <FormProvider {...confirmOrderForm}>
+      <CompleteOrderContainer
+        className="container"
+        onSubmit={handleSubmit(handleConfirmOrder)}
+      >
+        <CompleteOrderForm />
+        <SelectedCoffees />
+      </CompleteOrderContainer>
+    </FormProvider>
   )
 }

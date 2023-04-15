32bb0c9992fc0c00d4ab762b7cd158acf582505e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 22:11:58 -0300

    criando a função de resetar o carrinho

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -17,6 +17,7 @@ interface CartContextType {
   ) => void
   removeCartItem: (cartItemId: number) => void
   cartItemsTotal: number
+  cleanCart: () => void
 }
 
 interface CartContextProviderProps {
@@ -91,6 +92,10 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     setCartItems(newCart)
   }
 
+  function cleanCart() {
+    setCartItems([])
+  }
+
   useEffect(() => {
     localStorage.setItem(COFFEE_ITEMS_STORAGE_KEY, JSON.stringify(cartItems))
   }, [cartItems])
@@ -104,6 +109,7 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
         changeCartItemQuantity,
         removeCartItem,
         cartItemsTotal,
+        cleanCart,
       }}
     >
       {children}

c4b643b1869d532c2e7ae5113295ba6efa299771
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 21:29:51 -0300

    criando o righttext e o inputstyled

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,9 +1,28 @@
+/* eslint-disable react/display-name */
 import { InputHTMLAttributes, forwardRef } from 'react'
-import { InputStyleContainer } from './styles'
+import {
+  InputStyleContainer,
+  InputStyled,
+  InputWrapper,
+  RightText,
+} from './styles'
+import { RegularText } from '../Typography'
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string
+  rightText?: string
+}
 
-// eslint-disable-next-line react/display-name
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return <InputStyleContainer {...props} ref={ref} />
-})
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, rightText, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer hasError={!!error}>
+          <InputStyled {...props} ref={ref} />
+          {rightText && <RightText>{rightText}</RightText>}
+        </InputStyleContainer>
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>
+    )
+  },
+)

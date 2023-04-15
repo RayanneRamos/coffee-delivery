2b554637c9296082725c5fc4705e6f78fc2220c7
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 21:16:03 -0300

    adicionando os campos obrigatórios no form

--- src/pages/Checkout/components/AddressForm/index.tsx
+++ src/pages/Checkout/components/AddressForm/index.tsx
@@ -1,16 +1,67 @@
 import { Input } from '../../../../components/Input'
 import { AddressFormContainer } from './styles'
+import { useFormContext } from 'react-hook-form'
+
+interface ErrorsType {
+  errors: {
+    [key: string]: {
+      message: string
+    }
+  }
+}
 
 export function AddressForm() {
+  const { register, formState } = useFormContext()
+
+  const { errors } = formState as unknown as ErrorsType
+
   return (
     <AddressFormContainer>
-      <Input placeholder="CEP" className="cep" type="number" />
-      <Input placeholder="Rua" className="street" type="text" />
-      <Input placeholder="Número" type="number" />
-      <Input placeholder="Complemento" type="text" className="complement" />
-      <Input placeholder="Bairro" />
-      <Input placeholder="Cidade" />
-      <Input placeholder="UF" />
+      <Input
+        placeholder="CEP"
+        className="cep"
+        type="number"
+        {...register('cep')}
+        error={errors.cep?.message}
+      />
+      <Input
+        placeholder="Rua"
+        className="street"
+        type="text"
+        {...register('street')}
+        error={errors.street?.message}
+      />
+      <Input
+        placeholder="Número"
+        type="number"
+        {...register('number')}
+        error={errors.number?.message}
+      />
+      <Input
+        placeholder="Complemento"
+        type="text"
+        className="complement"
+        {...register('complement')}
+        error={errors.complement?.message}
+      />
+      <Input
+        placeholder="Bairro"
+        type="text"
+        {...register('district')}
+        error={errors.district?.message}
+      />
+      <Input
+        placeholder="Cidade"
+        type="text"
+        {...register('city')}
+        error={errors.city?.message}
+      />
+      <Input
+        placeholder="UF"
+        type="text"
+        {...register('uf')}
+        error={errors.uf?.message}
+      />
     </AddressFormContainer>
   )
 }

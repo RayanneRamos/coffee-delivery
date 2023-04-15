6db1ce57ba6e4dd6b61dbc609a483e93025243f8
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-04-14 22:01:28 -0300

    criando a validação da forma de pagamento

--- src/pages/Checkout/components/PaymentMethodOptions/index.tsx
+++ src/pages/Checkout/components/PaymentMethodOptions/index.tsx
@@ -1,12 +1,45 @@
+import { Bank, CreditCard, Money } from 'phosphor-react'
 import { PaymentMethodInput } from '../PaymentMethodInput'
 import { PaymentMedthodOptionsContainer } from './styles'
+import { useFormContext } from 'react-hook-form'
+import { RegularText } from '../../../../components/Typography'
+
+export const paymentMethods = {
+  credit: {
+    label: 'Cartão de Crédito',
+    icon: <CreditCard size={16} />,
+  },
+  debit: {
+    label: 'Cartão de Débito',
+    icon: <Bank size={16} />,
+  },
+  money: {
+    label: 'Dinheiro',
+    icon: <Money size={16} />,
+  },
+}
 
 export function PaymentMethodOptions() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext()
+  const paymentMethodError = errors?.paymentMethod?.message as unknown as string
   return (
     <PaymentMedthodOptionsContainer>
-      <PaymentMethodInput />
-      <PaymentMethodInput />
-      <PaymentMethodInput />
+      {Object.entries(paymentMethods).map(([key, { label, icon }]) => {
+        return (
+          <PaymentMethodInput
+            key={label}
+            icon={icon}
+            label={label}
+            value={key}
+            id={key}
+            {...register('paymentMethod')}
+          />
+        )
+      })}
+      {paymentMethodError && <RegularText>{paymentMethodError}</RegularText>}
     </PaymentMedthodOptionsContainer>
   )
 }
